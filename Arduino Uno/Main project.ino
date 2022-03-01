a9008e04596b936eb9393a5d146f309d0b72e56f
pushon <76487881+pushons@users.noreply.github.com>
2022-03-01 14:22:31 +0600

    Create Main project.ino

---
+++ Arduino Uno/Main project.ino
@@ -0,0 +1,10 @@
+// Daily Activities Routine with alarming including Date, Time slot & current weather.
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
